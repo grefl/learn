f6a795295ea3acf04eb3ad29e8895d558c8fca78
grefl <grefl@protonmail.com>
2022-05-15 11:36:29 +0200

    add another slow linear heap

--- algorithms/lectures/heap.py
+++ algorithms/lectures/heap.py
@@ -1,6 +1,8 @@
 
 
 # This heap is dump but it works
+# insertion is O(1)
+# remove_min is O(n)
 class DumbHeap():
 
     def __init__(self):
@@ -29,7 +31,7 @@ class DumbHeap():
 
 
 
-h = Heap()
+h = DumbHeap()
 h.insert(10)
 h.insert(20)
 h.insert(2)
@@ -37,4 +39,36 @@ print(h.remove_min())
 print(h.remove_min())
 h.insert(1)
 print(h.remove_min())
+
+def swap(array, i,j):
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+
+class DumbHeap2():
+    def __init__(self):
+        self.a = []
+        self.n = 0
+
+    # O(1)
+    def insert(self,x):
+        self.n +=1 
+        self.a.append(x)
+        i = 0 
+        while i < self.n-1 and (self.a[i] < self.a[i-1]):
+            swap(self.a, i,i-1)
+            i +=1
+        
+    # O(1)
+    def remove_min(self):
+        self.n -=1
+        return self.a.pop()
+h2 = DumbHeap2()
+h2.insert(10)
+h2.insert(20)
+h2.insert(2)
+print(h2.remove_min())
+print(h2.remove_min())
+h2.insert(1)
+print(h2.remove_min())
 
