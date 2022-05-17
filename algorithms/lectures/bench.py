4c72b2c0b547a957a48c7e2c3b27c835007dc6ff
grefl <grefl@protonmail.com>
2022-05-17 21:00:45 +0200

    benchmark

--- algorithms/lectures/bench.py
+++ algorithms/lectures/bench.py
@@ -2,6 +2,7 @@ import time
 import pathlib
 import random 
 from heap import DumbHeap, GoodHeap
+from merge import sort
 
 
 
@@ -16,13 +17,12 @@ print('----------------------------------------------------------')
 print('----------------------------BAD---------------------------')
 print('----------------------------------------------------------')
 st = time.monotonic()
-for i in range(50000):
+for i in range(500):
     h.insert(random.randint(0, 700000))
-for i in range(50000):
+for i in range(500):
     h.remove_min()
 et = time.monotonic() - st
 print(f"{et* 1000:.2f}: ms {(et/1000)* 1000}: seconds")
-
 
 
 gh = GoodHeap()
@@ -37,4 +37,26 @@ for i in range(50000):
 et = time.monotonic() - st
 print(f"{et* 1000:.2f}: ms {(et/1000)* 1000}: seconds")
 
+print('----------------------------------------------------------')
+print('----------------------------MERGE-------------------------')
+print('----------------------------------------------------------')
+st = time.monotonic()
+merge= []
+for i in range(50000):
+    merge.append(random.randint(0, 700000))
+sort(merge)
+et = time.monotonic() - st
+print(f"{et* 1000:.2f}: ms {(et/1000)* 1000}: seconds")
+
+
+print('----------------------------------------------------------')
+print('----------------------------INSERT------------------------')
+print('----------------------------------------------------------')
+st = time.monotonic()
+ch2 = []
+for i in range(50000):
+    ch2.append(random.randint(0, 700000))
+sorted(ch2)
+et = time.monotonic() - st
+print(f"{et* 1000:.2f}: ms {(et/1000)* 1000}: seconds")
 
