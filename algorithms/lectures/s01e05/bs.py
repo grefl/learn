ac9d98891e9d6721ce33df3eb80fdbc99c6de3ae
grefl <grefl@protonmail.com>
2022-05-21 19:21:21 +0200

    clean up

--- algorithms/lectures/s01e05/bs.py
+++ algorithms/lectures/s01e05/bs.py
@@ -1,9 +1,7 @@
-a = [2,11,30,31,40]
 
 # basic version
 def bs(a,l,r,x):
     m = (l+r) // 2
-    print(l,m,r)
     if a[m] == x:
         return m
     elif a[m] < x:
@@ -13,12 +11,9 @@ def bs(a,l,r,x):
     else:
         return None
 
-print(bs(a, 0, len(a)-1, 11))
 
 
 
-a = [2,11,30,31,40]
-x = -1 
 # gti = greater than or equal to i
 def bs_gti(a,x):
     l =  -1 
@@ -29,20 +24,11 @@ def bs_gti(a,x):
             r = m
         else:
             l = m
-    print(f"what is r{r}")
     return r 
 
 
-pos = bs_gti(a, x)
-
-if pos != len(a):
-    print("elements greater than i!")
-    print(a[pos:])
 
 # less than or equal to i
-
-a = [2,11,30,31,40]
-x = -1 
 # gti = greater than or equal to i
 def bs_lei(a,x):
     l =  -1 
@@ -55,8 +41,36 @@ def bs_lei(a,x):
             l = m
     return l 
 
-pos = bs_lei(a, 1000)
 
-if pos != -1:
-    print("elements less than!")
-    print(a[:pos])
+
+
+if __name__ == "__main__":
+
+    # ===========================
+    #   Basic Binary search 
+    # ===========================
+
+    a = [2,11,30,31,40]
+    print(bs(a, 0, len(a)-1, 11))
+    # ===========================
+    #    Greater than or equal 
+    # ===========================
+    a = [2,11,30,31,40]
+    x = -1 
+    pos = bs_gti(a, x)
+
+    if pos != len(a):
+        print("elements greater than i!")
+        print(a[pos:])
+
+    # ===========================
+    #     less than or equal
+    # ===========================
+    a = [2,11,30,31,40]
+    x = -1 
+
+    pos = bs_lei(a, 1000)
+
+    if pos != -1:
+        print("elements less than!")
+        print(a[:pos])
