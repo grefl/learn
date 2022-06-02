64c5a53f85c68494426556e7ba147fa6c94c9da5
grefl <grefl@protonmail.com>
2022-06-02 22:51:24 +0200

    add simple version

--- algorithms/leetcode/contains_duplicate.py
+++ algorithms/leetcode/contains_duplicate.py
@@ -19,13 +19,27 @@ def contains_duplicate(input):
         s.add(v) 
     return False 
 
+#===============================
+#     DIFFERENT SOLUTIONS
+#===============================
+def contains_duplicate_simple(input):
+    # s = set
+    # i = input
+    # Sets don't contain duplicates. 
+    # So, if we add all of i to s to s and check that 
+    # len(s) < len(i), if True then there are duplicates, 
+    # because we have only one reason to have less elements in
+    # s and i, that is, Sets don't contain duplicates
+    return len(set(input)) < len(input)
 
 class Test(unittest.TestCase):
 
     def test_should_be_true(self):
         input = [1,2,3,4,1]
         i_am_true = contains_duplicate(input)
+        i_am_also_true = contains_duplicate_simple(input)
         self.assertTrue(i_am_true)
+        self.assertTrue(i_am_also_true)
 
 
 
