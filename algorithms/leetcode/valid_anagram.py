7341b6da021de15fa9dada6aafc59e8fc1e764e0
grefl <grefl@protonmail.com>
2022-06-03 20:15:28 +0200

    version two, for fun

--- algorithms/leetcode/valid_anagram.py
+++ algorithms/leetcode/valid_anagram.py
@@ -16,6 +16,7 @@ def valid_anagram(s, t):
     if len(s) != len(t):
         # can't be an anagram if they're different lengths
         return False 
+
     m = {} 
     seen = set()
     for c in s:
@@ -30,14 +31,47 @@ def valid_anagram(s, t):
         m[c] -=1
         if m[c] == 0:
             seen.remove(c)
-            continue
 
 
     return len(seen) == 0
 
+def valid_anagram_v2(s, t):
+    """
+
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+    Example
+    -------------------------------------
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+    -------------------------------------
+    """
+
+    if len(s) != len(t):
+        # can't be an anagram if they're different lengths
+        return False 
+
+    m = {} 
+    count = len(s) 
+    for c in s:
+        if c not in m:
+            m[c] = 1
+        else:
+            m[c] += 1
+    for c in t:
+        if c not in m:
+            return False 
+        m[c] -=1
+        if m[c] < 0:
+            return False
+        count -=1
+
+    return count == 0
 
 if __name__ == "__main__":
-    s = "rat" 
-    t = "nat"
-    res = valid_anagram(s, t)
-    print(res)
+    s = "anagram" 
+    t = "annnnnn"
+    res_1 = valid_anagram(s, t)
+    res_2 = valid_anagram_v2(s, t)
+    print(res_1)
+    print(res_2)
