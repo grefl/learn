8dd7dc5c452c9c334ec782c76793662fe8e9838a
grefl <grefl@protonmail.com>
2022-06-04 21:26:02 +0200

    go fmt

--- algorithms/leetcode/valid_anagram.go
+++ algorithms/leetcode/valid_anagram.go
@@ -1,34 +1,35 @@
 package main
+
 import "fmt"
 
 func valid_anagram(s string, t string) bool {
-  m := make(map[rune]int)
-  if len(s) != len(t) {
-    return false
-  }
-  for _, c := range s {
-    if _, ok := m[c]; ok {
-      m[c] += 1
-    } else {
-      m[c] = 1
-    }
-  }
-  for _, c := range t {
-    if _, ok := m[c]; !ok {
-      return false 
-    }
-    m[c] -=1
+	m := make(map[rune]int)
+	if len(s) != len(t) {
+		return false
+	}
+	for _, c := range s {
+		if _, ok := m[c]; ok {
+			m[c] += 1
+		} else {
+			m[c] = 1
+		}
+	}
+	for _, c := range t {
+		if _, ok := m[c]; !ok {
+			return false
+		}
+		m[c] -= 1
 
-    if m[c] < 0 {
-      return false
-    }
+		if m[c] < 0 {
+			return false
+		}
 
-  }
-  return true 
+	}
+	return true
 }
 
 func main() {
-  s := "anagram"
-  t := "nagaram"
-  fmt.Println(valid_anagram(s, t))
+	s := "anagram"
+	t := "nagaram"
+	fmt.Println(valid_anagram(s, t))
 }
