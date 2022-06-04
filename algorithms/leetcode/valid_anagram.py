861cea3ca1138d7592fd196d0d99caed28b6bea4
grefl <grefl@protonmail.com>
2022-06-04 17:02:22 +0200

    more valid anagrams

--- algorithms/leetcode/valid_anagram.py
+++ algorithms/leetcode/valid_anagram.py
@@ -1,4 +1,4 @@
-
+from collections import Counter
 
 # Dumb version?
 def valid_anagram(s, t):
@@ -67,11 +67,37 @@ def valid_anagram_v2(s, t):
         count -=1
 
     return count == 0
+# Classic means the classic leetcode/normal way to answer this question
+def valid_anagram_classic(s, t):
+    if len(s) != len(t):
+        return False
+    m1 = {}
+    m2 = {}
+    for i in range(len(s)):
+        m1[s[i]] = 1 + m1.get(s[i], 0)
+        m2[t[i]] = 1 + m2.get(t[i], 0)
+
+    for c in s:
+        if m1[c] != m2.get(c, 0):
+            return False
+    return True 
+
+# How you would probably do this in the wild
+def valid_anagram_pythonic(s, t):
+    return Counter(s) == Counter(t)
+# O(1)
+s = "nag"
+t = "gan"
+s[0] = 12
 
 if __name__ == "__main__":
     s = "anagram" 
-    t = "annnnnn"
+    t = "nagaram"
     res_1 = valid_anagram(s, t)
     res_2 = valid_anagram_v2(s, t)
+    res_3 = valid_anagram_pythonic(s,t)
+    res_4 = valid_anagram_classic(s,t)
     print(res_1)
     print(res_2)
+    print(res_3)
+    print(res_4)
