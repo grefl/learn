d750f5b6f1d8fb9dbe33b6e615492097c5857213
grefl <grefl@protonmail.com>
2022-06-06 11:03:50 +0200

    heap version top k elements

--- algorithms/leetcode/top_k_frequent_elements.py
+++ algorithms/leetcode/top_k_frequent_elements.py
@@ -1,3 +1,4 @@
+import heapq
 def take_2nd(el):
     return el[1]
 def top_k_frequent_elements(nums, k):
@@ -26,13 +27,26 @@ def top_k_frequent_elements(nums, k):
     return [v for (v, k) in sorted(answer, key=take_2nd, reverse=True)][:k]
 
     
-
-
+def top_k_freq_elements_heap(nums, k):
+    m = {}
+    for n in nums:
+        if n not in m:
+            m[n] = 1
+        else:
+            m[n] +=1
+    max_heap = [(-freq, val) for val, freq in m.items()]
+    heapq.heapify(max_heap) 
+    answer = []
+    while len(answer) < k:
+        answer.append(heapq.heappop(max_heap)[1])
+    return answer
 
 if __name__ == "__main__":
     nums = [4,1,-1,2,-1,2,3] 
     k = 2
-    res = top_k_frequent_elements(nums, k)
-    print(res)
+    res1 = top_k_frequent_elements(nums, k)
+    res2 = top_k_freq_elements_heap(nums, k)
+    print(res1)
+    print(res2)
    
 
