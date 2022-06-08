48e4909e65e35cf5540f25ea98d5aebe02d6b3bd
grefl <grefl@protonmail.com>
2022-06-08 23:36:14 +0200

    working version of max_sub_array

--- algorithms/leetcode/max_sub_array.py
+++ algorithms/leetcode/max_sub_array.py
@@ -1,15 +1,18 @@
-import sys
+import math
 
-def max_sub_array(array):
-    best = current = -sys.maxint 
+def max_sub_array(nums):
+    if len(nums) == 1:
+        return nums[0]
 
+    best = -math.inf
+    current = 0
     for num in nums:
-        current = max(-sys.maxint, num + current) 
-        best    = max(best, current) 
+        current += num
+        best    = max(best, current)
+        current = max(0, current)
     return best
 
 
 
-
-print(max_sub_array())
+print(max_sub_array([-2,1,-3,4,-1,2,1,-5,4]))
 
