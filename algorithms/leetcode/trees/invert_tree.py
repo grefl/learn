1ddae5db873c43dabe9795dc0b7c664854ab1bb4
grefl <grefl@protonmail.com>
2022-07-11 21:28:58 +0000

    add iterative version

--- algorithms/leetcode/trees/invert_tree.py
+++ algorithms/leetcode/trees/invert_tree.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import deque
 
 def invertTree(root):
     """
@@ -19,6 +20,22 @@ def invertTree(root):
     root.right = temp 
     invertTree(root.left)
     invertTree(root.right)
+    return root
+
+def invertTreeIterative(root):
+    if root is None:
+        return None
+    q = deque()
+    q.append(root)
+    while q:
+        node = q.popleft()
+        temp = node.left
+        node.left = node.right
+        node.right = temp
+        if node.left is not None:
+            q.append(node.left) 
+        if node.right is not None:
+            q.append(node.right) 
     return root
 
 def unwind(root):
@@ -48,6 +65,11 @@ class Test(unittest.TestCase):
         tree = TreeNode(4, TreeNode(2), TreeNode(7))
         invertTree(tree)
         self.assertEqual([4,7,2], unwind(tree))
+    def test_2(self):
+        tree = TreeNode(4, TreeNode(2, TreeNode(1)), TreeNode(7))
+        invertTreeIterative(tree)
+
+        self.assertEqual([4,7,2,1], unwind(tree))
 
 if __name__ == "__main__":
     unittest.main()
