c1b58dfdec838135de1e6148fbae8647ee766c82
grefl <grefl@protonmail.com>
2022-07-15 20:33:34 +0200

    even better

--- algorithms/mine/dumb_hashtable.py
+++ algorithms/mine/dumb_hashtable.py
@@ -2,16 +2,21 @@ import unittest
 
 
 class DumbHashTable:
+
     def __init__(self, size=10):
         self.array = [[(None, None)] for _ in range(size)]
-        print(self.array)
         self.size = size
-    def getval(self,  array, current_key):
+        self.inserts = 0
 
+    def should_resize(self):
+        return  (self.inserts / self.size) > 0.8
+
+    def getval(self,  array, current_key):
         for (key, value) in array:
             if key == current_key:
                 return value
         return None
+
     def exists(self, array, current_key):
         for index,(key, _) in enumerate(array):
             if key == current_key:
@@ -19,21 +24,36 @@ class DumbHashTable:
         return None 
 
     def hash(self, current_key):
-        hashed_key = sum([ord(c) for c in current_key]) % self.size
+        hashed_key = (sum([ord(c) for c in current_key]) * 17)  % self.size
         return hashed_key
+
     def insert(self, current_key, value):
         hashed_key = self.hash(current_key)
         index = self.exists(self.array[hashed_key], current_key)
+
         if index is None:
-            self.array[hashed_key].append((current_key, value))
+            if not self.should_resize():
+                self.inserts +=1 
+                self.array[hashed_key].append((current_key, value))
+            else:
+                # potentially expensive operation
+
+                old_array = self.array
+                self.size = self.size * 10 + self.inserts
+                self.inserts = 0
+                self.array= [[(None, None)] for _ in range(self.size)]
+                for inner_array in old_array:
+                    for (prev_key, prev_value) in inner_array:
+                        if prev_key is None:
+                            continue
+                        self.insert(prev_key, prev_value)
         else:
             self.array[hashed_key][index] = (current_key, value) 
-        
         return value
+
     def get(self, current_key):
         hashed_key = self.hash(current_key)
         array = self.array[hashed_key]
-        print(f"{array=}")
         value = self.getval(array, current_key)
         return value 
 
@@ -47,9 +67,6 @@ class Test(unittest.TestCase):
         self.assertEqual(h.get('gorb'), "I am GORB!") 
 
         h.insert("hi", 1)
-        self.assertEqual(h.get('hi'), 1)
-        for i in range(100):
-            h.insert(str(i), i)
         self.assertEqual(h.get('hi'), 1)
 
 if __name__ == "__main__":
