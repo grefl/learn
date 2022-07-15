1072f568d76361e60d014aaaef5ff0721c7d8e8f
grefl <grefl@protonmail.com>
2022-07-15 19:39:04 +0200

    less dumb

--- algorithms/mine/dumb_hashtable.py
+++ algorithms/mine/dumb_hashtable.py
@@ -3,22 +3,38 @@ import unittest
 
 class DumbHashTable:
     def __init__(self, size=10):
-        self.array = [(None, None)] * size
+        self.array = [[(None, None)] for _ in range(size)]
+        print(self.array)
         self.size = size
+    def getval(self,  array, current_key):
+
+        for (key, value) in array:
+            if key == current_key:
+                return value
+        return None
+    def exists(self, array, current_key):
+        for index,(key, _) in enumerate(array):
+            if key == current_key:
+                return index 
+        return None 
 
     def hash(self, current_key):
         hashed_key = sum([ord(c) for c in current_key]) % self.size
         return hashed_key
     def insert(self, current_key, value):
         hashed_key = self.hash(current_key)
-        self.array[hashed_key] = (current_key, value)
+        index = self.exists(self.array[hashed_key], current_key)
+        if index is None:
+            self.array[hashed_key].append((current_key, value))
+        else:
+            self.array[hashed_key][index] = (current_key, value) 
+        
         return value
     def get(self, current_key):
         hashed_key = self.hash(current_key)
-        (prev_key, value) = self.array[hashed_key]
-        if prev_key is None or prev_key != current_key:
-            return None
-        (_, value) = self.array[hashed_key]
+        array = self.array[hashed_key]
+        print(f"{array=}")
+        value = self.getval(array, current_key)
         return value 
 
             
@@ -34,7 +50,7 @@ class Test(unittest.TestCase):
         self.assertEqual(h.get('hi'), 1)
         for i in range(100):
             h.insert(str(i), i)
-        self.assertEqual(h.get('hi'), None)
+        self.assertEqual(h.get('hi'), 1)
 
 if __name__ == "__main__":
     unittest.main()
