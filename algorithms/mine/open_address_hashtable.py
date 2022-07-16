5d033490e6b611d0c113c2225e327abcdeadea88
grefl <grefl@protonmail.com>
2022-07-16 16:45:03 +0200

    basic version not very fast

--- algorithms/mine/open_address_hashtable.py
+++ algorithms/mine/open_address_hashtable.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 class HashTable:
     def __init__(self, size = 100):
-        self.size = size * 2
+        self.size = size * 10 
         self.array = [None] * self.size
 
         self.large_int       = self.gen_large_int()
@@ -16,10 +16,10 @@ class HashTable:
 
         k = sum([ord(c) for c in current_key])
         hashed_key = ((k*self.A) % self.P) % self.size
-        return hashed_key
+        return hashed_key 
 
     def gen_large_int(self):
-        return random.randint(2**16, 2**32)
+        return random.randint(0, 2**128)
 
     def gen_large_prime(self, large_int):
         number = large_int
@@ -49,6 +49,16 @@ class Test(unittest.TestCase):
         h.insert("Hollow knight", "10/10")
         h.insert("GORB1", "I am not GORB!")
         self.assertEqual(h.get("GORB"), "I am GORB!")
+    def test_2(self):
+        words = Path('./words.txt').read_text().split('\n')[:1000]
+        h = HashTable(len(words))
+        print(h.size)
+        for word in words:
+            h.insert(word, word)
+
+        for word in words:
+            v = h.get(word)
+        print(h.array)
 
 if __name__ == "__main__":
     unittest.main()
