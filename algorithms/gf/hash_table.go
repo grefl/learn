a1e7be5b495dfbe05186ddf35c8777bfbdc911fc
grefl <grefl@protonmail.com>
2022-07-17 17:35:02 +0200

    fmt

--- algorithms/gf/hash_table.go
+++ algorithms/gf/hash_table.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
-  "time"
 	"strings"
+	"time"
 )
 
 func bench(compute func(), count int) {
-  for i := 0; i < count; i +=1 {
-    start := time.Now()
-    compute()
-    t := time.Now()
-    elapsed := t.Sub(start)
-    fmt.Println(elapsed)
-    fmt.Println(count)
-  }
+	for i := 0; i < count; i += 1 {
+		start := time.Now()
+		compute()
+		t := time.Now()
+		elapsed := t.Sub(start)
+		fmt.Println(elapsed)
+		fmt.Println(count)
+	}
 }
 
 const S uint = 7
@@ -32,9 +32,9 @@ func take(array []string, amount int) []string {
 func (h *HashTable) hash(key string) uint {
 	hashed_key := S
 	for _, c := range key {
-    hashed_key += ((hashed_key * 31) + uint(c)) % uint(len(h.array))
+		hashed_key += ((hashed_key * 31) + uint(c)) % uint(len(h.array))
 	}
-  return hashed_key % uint(len(h.array))
+	return hashed_key % uint(len(h.array))
 }
 
 type HashTable struct {
@@ -59,7 +59,7 @@ func (h *HashTable) insert(current_key, current_value string) bool {
 func (h *HashTable) get(current_key string) (string, bool) {
 	hashed_index := h.hash(current_key)
 	for len(h.array[hashed_index]) == 0 {
-    hashed_index = (hashed_index + 1) % uint(len(h.array))
+		hashed_index = (hashed_index + 1) % uint(len(h.array))
 	}
 	if h.array[hashed_index][0] == current_key {
 		return h.array[hashed_index][1], true
@@ -69,35 +69,34 @@ func (h *HashTable) get(current_key string) (string, bool) {
 
 func test(words []string) {
 
-
 	// words_100 := take(words, 100000)
-  array := make([][]string, len(words)*2)
+	array := make([][]string, len(words)*2)
 	for i, _ := range array {
 		array[i] = make([]string, 2)
 	}
 	h := HashTable{array: array, capacity: 0}
 	fmt.Println(h.size())
-  h.insert("gorb", "me")
+	h.insert("gorb", "me")
 	h.insert("GORB!", "I am GORB!")
-  for _, word := range words {
+	for _, word := range words {
 		h.insert(word, word)
 	}
-  res, ok := h.get("gorb")
+	res, ok := h.get("gorb")
 	if !ok {
 		log.Fatal("couldn't find key")
 	}
-  people, ok := h.get("people")
+	people, ok := h.get("people")
 	if !ok {
 		log.Fatal("couldn't find key")
 	}
 	fmt.Println(res)
-  fmt.Println(people)
+	fmt.Println(people)
 }
 func main() {
-  data, err := os.ReadFile("./words.txt")
-  if err != nil {
-    log.Fatal(err)
-  }
-  words := strings.Split(string(data), "\n")
-  bench( func() {test(words)}, 10)
+	data, err := os.ReadFile("./words.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	words := strings.Split(string(data), "\n")
+	bench(func() { test(words) }, 10)
 }
